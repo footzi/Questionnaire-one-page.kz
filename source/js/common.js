f69ad3c8c08c527dfcf4048de76cf7a702630209
footzi <footzi@mail.ru>
2017-07-13 20:57:28 +0300

    last edits

--- source/js/common.js
+++ source/js/common.js
@@ -1,32 +1,44 @@
+//открытие меню
 $(".nav-button").on("click", function() {
     $(".header-nav").fadeToggle();
+});
+$(".header-nav a").on("click", function(e) {
+    if ($(window).width() <= 768) {
+        $(".header-nav").hide();
+    }
 })
 
+//плавная прокрутка
+$(".header-nav a").on("click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr("data-scroll");
+    var top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+})
 
+//slider
 $(".slider").slider({
     min: 0,
     max: 100,
     range: "min",
-    value: 20,
+    value: 35,
     step: 5,
     animate: "slow",
-    slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value + "%");
-      }
+    slide: function(event, ui) {
+        $("#amount").val(ui.value + "%");
+    }
 })
-
 $(".slider-v").slider({
     orientation: "vertical",
     min: 0,
     max: 100,
     range: "min",
-    value: 20,
+    value: 35,
     step: 5,
     animate: "fast",
-    slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value + "%");
-      }
+    slide: function(event, ui) {
+        $("#amount").val(ui.value + "%");
+    }
 })
-
-$("#amount").val($( ".slider" ).slider( "value")+"%");
-$("#amount2").val($( ".slide-v" ).slider( "value")+"%");
+$("#amount").val($(".slider").slider("value") + "%");
+$("#amount2").val($(".slide-v").slider("value") + "%");
